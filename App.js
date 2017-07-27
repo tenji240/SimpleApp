b6b30444c097d30c614f8db2e6450f5a3813dcdf
Tenji Tembo <tenji240@gmail.com>
2017-07-27 16:10:17 -0400

    Initial Commit

---
+++ App.js
@@ -0,0 +1,171 @@
+import GiftedMessenger from 'react-native-gifted-messenger'
+import React from 'react';
+import { AppRegistry, Text, View, Button, Dimensions, FlatList,} from 'react-native';
+import {StackNavigator, TabNavigator} from 'react-navigation';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+//Imports
+import MY_CONTACTS from './src/data/contacts.json';
+import styles from './src/mincss/minspec.json';
+
+class RecentChatsScreen extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>List of Recent Chats</Text>
+        <FlatList 
+          data={MY_CONTACTS}
+          renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>} />
+        <Button onPress={() => this.props.navigation.navigate('Chat', { user: 'Lucy' })}
+          title="Chat with Lucy"/>
+      </View>
+    );
+  }
+}
+
+//Settings for Recent Chat
+RecentChatsScreen.navigationOptions = {
+  tabBarLabel: 'Recent',
+  tabBarIcon: <Ionicons name={'ios-time-outline'} size={26} style={{color: '#2980b9'}}/>
+};
+
+class AllContactsScreen extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Contact List</Text>
+        <FlatList
+          data={MY_CONTACTS}
+          renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+        />
+      </View>
+    );
+  }
+}
+
+// Settings for All Contacts
+AllContactsScreen.navigationOptions = {
+  tabBarLabel: 'Contacts',
+  tabBarIcon: <Ionicons name={'ios-contact-outline'} size={26} style={{ color: '#2980b9' }} />,
+};
+
+class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Welcome',
+  };
+
+  render() {
+    const { navigate } = this.props.navigation;
+    return (
+      <View>
+        <Text>Hello, Chat App!</Text>
+      </View>
+    );
+  }
+}
+
+//Settings for Home Screen
+HomeScreen.navigationOptions = {
+  tabBarLabel: 'Home',
+  tabBarIcon: <Ionicons name={'ios-home-outline'} size={26} style={{ color: '#2980b9' }} />,
+};
+
+
+//Main Tab Navigation
+const MainScreenNavigator =  TabNavigator({
+  Home: {screen: HomeScreen },
+  Recent: {screen: RecentChatsScreen}, 
+  All: {screen: AllContactsScreen },
+});
+
+MainScreenNavigator.navigationOptions = {
+  title: 'SimpleApp',
+};
+
+//Temp Chat Screen Component
+class ChatScreen extends React.Component {
+  static navigationOptions = ({ navigation }) => {
+    const {state, setParams} =  navigation;
+    const isInfo = state.params.mode === 'info';
+    const {user} = state.params;
+    
+    return {
+      title: isInfo ? `${user}'s Contact Info` : `Chatting with ${state.params.user}`,
+      headerRight: (
+        <Button title={ isInfo ? 'Done' : `${user}'s Info` }
+          onPress={() => setParams({mode: isInfo ? 'none' : 'info'})}/>
+      ),
+    };
+  };
+  
+  render() {
+    const { params } = this.props.navigation.state;
+    return (
+      <View style={{
+          flex: 1,
+          marginTop: 20,
+        }}>
+        <GiftedMessenger
+          ref={(c) => this._GiftedMessenger = c}
+          styles={{
+            container: {
+              width: Dimensions.get('window').width,
+            },
+            bubbleRight: {
+              marginLeft: 70,
+              backgroundColor: "#007aff",
+            },
+          }}
+          autoFocus={false}
+          messages={[
+            {
+              text: 'Are you building a chat app?',
+              name: 'React-Bot',
+              image: {uri: 'https://facebook.github.io/react/img/logo_og.png'},
+              position: 'left',
+              date: new Date(2016, 3, 14, 13, 0),
+              uniqueId: Math.round(Math.random() * 10000), // simulating server-side unique id generation
+            },
+            {
+              text: "Yes, and I use Gifted Messenger!",
+              name: 'Awesome Developer',
+              image: null,
+              position: 'right',
+              date: new Date(2016, 3, 14, 13, 1),
+              uniqueId: Math.round(Math.random() * 10000), // simulating server-side unique id generation
+            },
+          ]}
+          handleSend={() => {}} //push message to json stack trace or locally save it
+          onErrorButtonPress={() => {}}
+          maxHeight={Dimensions.get('window').height - 20}
+      
+          loadEarlierMessagesButton={false}
+          onLoadEarlierMessages={() => {}}
+      
+          senderName="Becky"
+          senderImage={null}
+          onImagePress={() => {}}
+          displayNames={true
+          }
+    
+          parseText={true}
+          handlePhonePress={() => {}}
+          handleUrlPress={() => {}}
+          handleEmailPress={() => {}}
+      
+          isLoadingEarlierMessages={false}
+      
+          typingMessage={"typing..."}
+        />
+      </View>
+    );
+  }
+}
+
+//Main App Setup
+const SimpleApp = StackNavigator({
+  Home: { screen: MainScreenNavigator },
+  Chat: { screen: ChatScreen },
+});
+
+AppRegistry.registerComponent('SimpleApp', () => SimpleApp);
---
+++ README.md
@@ -0,0 +1,29 @@
+## Demo React Chat App README
+
+#### Before starting
+- make sure you have Xcode installed and up to date to the latest CLI
+- update brew
+- install brew and watchman: `brew install node && brew install watchman`
+- install react native cli: `npm install -g react-native-cli`
+
+### Running the project
+- Clone repo
+- `cd SimpleApp/`
+- `npm install`
+- `react-native run-ios`
+- Bingo
+
+### Deco - React Native IDE
+- I have been using this IDE lately: [Deco](https://www.decoide.org/) for ReactNative Dev.
+- It includes and auto-install components while also keeping `package.json` updated
+- After installing Deco, add the repo to the project view
+- Hit the Simulator button, select an iOS device, and voila
+
+### Troubleshooting
+- `Unexpected Character in whatever.png` - you'll have to restart the packager. If it's running via terminal, kill the process and restart it. If it's running via Deco, kill Deco and restart. If it doesn't work, restart your computer
+
+
+### Relevant Documentation
+- [React](https://facebook.github.io/react/)
+- [React Native](https://facebook.github.io/react-native/)
+- [React Navigator](https://reactnavigation.org/)
